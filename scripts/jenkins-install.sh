85981cd936e56ed4485f9d71733261e33f7d4fd9
DAYANAND DEEPAK <30375388+daya1994@users.noreply.github.com>
2026-09-10 23:42:50 +0530

    Add Jenkins installation script

---
+++ scripts/jenkins-install.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/rpm-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/rpm-stable/jenkins.io-2026.key
+sudo yum install java-21-amazon-corretto -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
